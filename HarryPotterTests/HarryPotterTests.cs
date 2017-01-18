7b58f14da1c664e406306fa4cf0833ce61044875
曾建凱 <kai_tseng@huanuage.com>
2017-01-18 09:35:14 +0800

    GetCostTest_1本第1_2_3_4集_320元(失敗)

--- HarryPotterTests/HarryPotterTests.cs
+++ HarryPotterTests/HarryPotterTests.cs
@@ -56,5 +56,22 @@ namespace HarryPotter.Tests
 
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod()]
+        public void GetCostTest_1本第1_2_3_4集_320元()
+        {
+            List<IHarryPotterBook> books = new List<IHarryPotterBook> {
+                new HarryPotterBook1 { count = 1 },
+                new HarryPotterBook2 { count = 1 },
+                new HarryPotterBook3 { count = 1 },
+                new HarryPotterBook4 { count = 1 }
+            };
+            HarryPotter target = new HarryPotter();
+
+            decimal expected = 320;
+            var actual = target.GetCost(books);
+
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
