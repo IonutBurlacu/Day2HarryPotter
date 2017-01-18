1eebd470371779e72325cfa93f7d9568f8e91802
曾建凱 <kai_tseng@huanuage.com>
2017-01-18 09:28:07 +0800

    GetCostTest_1本第1集_1本第2集_190元(成功)

--- HarryPotter/HarryPotter.cs
+++ HarryPotter/HarryPotter.cs
@@ -12,7 +12,14 @@ namespace HarryPotter
         public decimal GetCost(IEnumerable<IHarryPotterBook> books)
         {
             decimal cost = 0;
-            cost = books.Sum(b => b.count * b.price);
+            if (books.Count() == 2)
+            {
+                cost = books.Sum(b => b.count * b.price) * (decimal)0.95;
+            }
+            else
+            {
+                cost = books.Sum(b => b.count * b.price);
+            }
             return cost;
         }
     }
